8af0896db1cd70feff2a03b9b4ff70d4c3075393
xuerou0219 <127731126+xuerou0219@users.noreply.github.com>
2023-12-07 16:04:07 +0800

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CBF110049_NextButton0_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void CBF110049_DDL1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        CBF110049_cambridge.Text = string.Format($"{CBF110049_DDL1.SelectedValue}=>{CBF110049_GV1.DataSource}");
+    }
+
+    protected void CBF110049_testBtn0_Click(object sender, EventArgs e)
+    {
+        CBF110049_MV1.ActiveViewIndex = 0;
+        CBF110049_MV1.ActiveViewIndex = 1;
+    }
+}
